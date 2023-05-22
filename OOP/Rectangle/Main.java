8ad1b6bd5bf5621d4bca910fa95929856c6ea04d
slany3.jan <97552526+slanja@users.noreply.github.com>
2023-05-22 14:25:57 +0200

    Create Main.java

---
+++ OOP/Rectangle/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Rectangle rec = new Rectangle(5, 3);
+
+        System.out.println("Perimeter is: " + rec.RectanglePerimeter() + "\nArea is: " + rec.RectangleArea());
+
+    }
+}
