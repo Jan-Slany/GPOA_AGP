a07e73810863da2f6efecec60b2d83ba0ecd3e38
slany3.jan <97552526+slanja@users.noreply.github.com>
2023-05-22 16:01:37 +0200

    Create BankAccount.java

---
+++ OOP/BankAccount/BankAccount.java
@@ -0,0 +1,50 @@
+public class BankAccount {
+    private int AccountNumber;
+    private String AccountOwner;
+    private int AccountBalance;
+
+    public BankAccount(int num, String owner, int balance) {
+        this.AccountNumber = num;
+        this.AccountOwner = owner;
+        this.AccountBalance = balance;
+    }
+
+    // setting values
+    public void setAccountNumber(int accountNumber) {
+        AccountNumber = accountNumber;
+    }
+
+    public void setAccountOwner(String accountOwner) {
+        AccountOwner = accountOwner;
+    }
+
+    public void setAccountBalance(int accountBalance) {
+        AccountBalance = accountBalance;
+    }
+
+    // getting values
+    public int getAccountNumber() {
+        return AccountNumber;
+    }
+
+    public String getAccountOwner() {
+        return AccountOwner;
+    }
+
+    public int getAccountBalance() {
+        return AccountBalance;
+    }
+
+    // functions
+    // requiring account number to verify bank account
+    public int Deposit(int amount, int AccountNum) {
+        if (this.AccountNumber == AccountNum) return this.AccountBalance += amount;
+        else return this.AccountBalance;
+    }
+
+    // requiring account number to verify bank account
+    public int Withdraw(int amount, int AccountNum) {
+        if (this.AccountNumber == AccountNum) return this.AccountBalance -= amount;
+        else return this.AccountBalance;
+    }
+}
