500abbeb271fbd66ae48aa94503ea81a8b65e47d
slany3.jan <97552526+slanja@users.noreply.github.com>
2023-05-29 13:13:28 +0200

    Update Circle.java

--- OOP/Circle/Circle.java
+++ OOP/Circle/Circle.java
@@ -1,45 +1,26 @@
 public class Circle {     
-
-  private int radius;    
+  private int radius;  
+  
+  public Circle(int radius) {         
+    this.radius = radius;     
+  }     
+  
+  // setting values     
+  public void setRadius(int radius) {         
+    this.radius = radius;     
+  }     
+  
+  // getting values     
+  public int getRadius() {         
+    return radius;     
+  }     
+  
+  // functions     
+  @Override     
+  public String toString() {         
+    return "Radius: " + this.radius;     
+  } 
+}   
 
      
 
-  public Circle(int radius) {         
-
-      this.radius = radius;    
-
-   }   
-
-      
-
-   // setting values     
-
-   public void setRadius(int radius {         
-
-       this.radius = radius;    
-
-    }   
-
-        
-
-    // getting values     
-
-    public int getRadius() {         
-
-        return radius;     
-
-    }    
-
-       
-
-    // functions     
-
-    @Override     
-
-    public String toString() {         
-
-        return "Radius: " + this.radius;     
-
-    } 
-
-}
