811fb5d09e415d8c93b1d5e3ae8fb13786683eb1
slany3.jan <97552526+slanja@users.noreply.github.com>
2023-05-29 13:10:38 +0200

    Create Circle.java

---
+++ OOP/Circle/Circle.java
@@ -0,0 +1,45 @@
+public class Circle {     
+
+  private int radius;    
+
+     
+
+  public Circle(int radius) {         
+
+      this.radius = radius;    
+
+   }   
+
+      
+
+   // setting values     
+
+   public void setRadius(int radius {         
+
+       this.radius = radius;    
+
+    }   
+
+        
+
+    // getting values     
+
+    public int getRadius() {         
+
+        return radius;     
+
+    }    
+
+       
+
+    // functions     
+
+    @Override     
+
+    public String toString() {         
+
+        return "Radius: " + this.radius;     
+
+    } 
+
+}
