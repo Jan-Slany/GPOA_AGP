3d417f1088687c43e8d058ec4477c65f36b1e4e6
slany3.jan <97552526+slanja@users.noreply.github.com>
2023-05-30 16:47:07 +0200

    Update Main.java

--- OOP/Car/Main.java
+++ OOP/Car/Main.java
@@ -1,14 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        Car car = new Car("Nissan", "Terrano II", 1993, 124132);
+        Car car = new Car("Skoda", "Octavia");
 
-        System.out.println(
-                "Car brand: " + car.getBrand() +
-                "\nCar model: " + car.getModel() +
-                "\nYear of manufacture: " + car.getManufactureYear() +
-                "\nKilometres driven: " + car.getKilometresDriven()
-        );
-
-        System.out.printf("\nAverage consumption per 100 km: %.2f l", car.AverageFuelConsumption(100, car.getKilometresDriven()));
+        System.out.println(car);
     }
 }
