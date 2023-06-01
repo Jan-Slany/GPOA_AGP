8f02c46c16b083128ca869c138a040349c2c4461
slany3.jan <97552526+slanja@users.noreply.github.com>
2023-06-01 20:33:43 +0200

    Create Main.java

---
+++ OOP/Address/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Address address = new Address("Um die Ecke", 24, "Mytown", 324);
+
+        System.out.println(address);
+    }
+}
