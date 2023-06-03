b3acc2937a416827fff56f707d7fd2219a66edd3
slany3.jan <97552526+slanja@users.noreply.github.com>
2023-06-03 22:49:43 +0200

    Update Main.java

--- OOP/BankAccount/Main.java
+++ OOP/BankAccount/Main.java
@@ -2,25 +2,25 @@ public class Main {
     public static void main(String[] args) {
         // declaring new bank account
         Person person = new Person("Jan", "Slaný");
-        BankAccount bank = new BankAccount(1324, person);
+        BankAccount myAccount = new BankAccount(1324, person);
 
         // all account details
-        System.out.println(bank);
+        System.out.println(myAccount);
 
         // if you type correct account number, the transaction will progress
-        bank.Deposit(1000000, 1324);
-        System.out.println("Balance: " + bank.getAccountBalance() + "\n");
+        myAccount.Deposit(1000000, 1324);
+        System.out.println("Balance: " + myAccount.getAccountBalance() + "\n");
 
         // if type incorrect, nothing will happen
-        bank.Deposit(1000000, 1224);
-        System.out.println("Balance: " + bank.getAccountBalance() + "\n");
+        myAccount.Deposit(1000000, 1224);
+        System.out.println("Balance: " + myAccount.getAccountBalance() + "\n");
 
         // withdrawing money
-        bank.Withdraw(500000,1324);
-        System.out.println("Balance: " + bank.getAccountBalance() + "\n");
+        myAccount.Withdraw(500000,1324);
+        System.out.println("Balance: " + myAccount.getAccountBalance() + "\n");
 
         // interest rate
-        System.out.println("Interest is: " + bank.InterestRate(12000, 2, 12));
+        System.out.println("Interest is: " + myAccount.InterestRate(12000, 2, 12));
 
         // creating new bank account with account number 123456
         Person owner = new Person("Unknown", "User");
@@ -28,5 +28,14 @@ public class Main {
 
         // printing account details of account 123456
         System.out.println(user);
+
+
+        // setting address
+        Address address = new Address("Beautiful", 3, "Beautiful city", 3642);
+
+        // printing info about bank
+        BankAccount bank = new BankAccount("Czech Saving Bank", address.FullAddress());
+
+        System.out.println(bank.BankInfo());
     }
 }
